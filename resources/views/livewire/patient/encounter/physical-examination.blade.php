8cdddd2831cddee87961c13f13dea1b06907557f
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-03-19 23:40:44 +0800

    update

--- resources/views/livewire/patient/encounter/physical-examination.blade.php
+++ resources/views/livewire/patient/encounter/physical-examination.blade.php
@@ -11,7 +11,14 @@ state([
 ]);
 ?>
 
-<div>
+<div class="relative">
+    <button type="button" class="btn btn-info m-1 font-medium underline absolute right-0 top-2" x-data=""
+        x-on:click="$dispatch('open-modal', 'create-new-allergy')">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+            <path
+                d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+        </svg>
+    </button>
     <h3 class="text-xl font-bold text-navy-700 dark:text-white">{{ __('Physical Exam') }}</h3>
     <x-table for="physical_exam">
         <x-table.tbody class="dark:border-gray-500">
