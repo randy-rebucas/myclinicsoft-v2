7768a70801d5305b625a3f4a21a0b58c6967dd92
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-05-06 09:22:06 +0800

    setup completion

--- resources/views/livewire/setup/index.blade.php
+++ resources/views/livewire/setup/index.blade.php
@@ -1,8 +1,17 @@
 <?php
 
-use function Livewire\Volt\{state, layout};
+use App\Livewire\Forms\SetupForm;
+use function Livewire\Volt\{state, layout, form};
 
-layout('layouts.app');
+form(SetupForm::class);
+
+layout('layouts.guest');
+
+$save = function () {
+
+    $this->form->store();
+
+};
 
 ?>
 
@@ -14,6 +23,53 @@ layout('layouts.app');
     </x-slot>
 
     <div class="py-12">
-        <p>Setup</p>
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Let`s setup your app.') }}
+        </div>
+    
+        <form wire:submit="save">
+            <!-- Name -->
+            <div>
+                <x-input-label for="name" :value="__('Name')" />
+                <x-text-input wire:model="form.name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
+                <x-input-error :messages="$errors->get('form.name')" class="mt-2" />
+            </div>
+    
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
+                <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+            </div>
+    
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+    
+                <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+    
+                <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
+            </div>
+    
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    
+                <x-text-input wire:model="form.password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required autocomplete="new-password" />
+    
+                <x-input-error :messages="$errors->get('form.password_confirmation')" class="mt-2" />
+            </div>
+    
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button class="ms-4">
+                    {{ __('Submit') }}
+                </x-primary-button>
+            </div>
+        </form>
     </div>
 </div>
