c1aa23e2b20feb5c21be01a98b3dec99de4e0533
Randy <rebucasrandy1986@gmail.com>
2024-10-16 13:57:35 +0800

    update dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -18,7 +18,7 @@ new #[Layout('layouts.app')] class extends Component {
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Welcome back!") }}
                 </div>
             </div>
         </div>
