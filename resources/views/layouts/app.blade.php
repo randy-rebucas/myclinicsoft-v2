9f1ac4e2d9fda698d14921d6c14e7a5c2f3c92fd
Randy <rebucasrandy1986@gmail.com>
2024-11-20 20:56:05 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 <body class="font-sans antialiased h-full">
     <div class="min-h-screen">
         <!-- Wave Background -->
-        <div class="absolute inset-0 bg-gradient-to-br from-indigo-50 to-pink-50 overflow-hidden -z-10">
+        <div class="fixed inset-0 bg-gradient-to-br from-indigo-50 to-pink-50 overflow-hidden -z-10">
             <!-- Top Wave -->
-            <svg class="absolute top-0 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+            <svg class="absolute top-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="xMidYMax slice">
                 <path fill="rgba(99, 102, 241, 0.1)" fill-opacity="1" d="M0,128L48,144C96,160,192,192,288,186.7C384,181,480,139,576,138.7C672,139,768,181,864,186.7C960,192,1056,160,1152,144C1248,128,1344,128,1392,128L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
                 <path fill="rgba(99, 102, 241, 0.05)" fill-opacity="1" d="M0,160L48,144C96,128,192,96,288,106.7C384,117,480,171,576,181.3C672,192,768,160,864,154.7C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
             </svg>
