977a216bac841eeff898a25f39f7a55ce1f90fbb
Randy <rebucasrandy1986@gmail.com>
2024-11-21 16:19:06 +0800

    update

--- resources/views/livewire/patient/index.blade.php
+++ resources/views/livewire/patient/index.blade.php
@@ -79,7 +79,7 @@ on(['set-date' => function ($date) {
                 </x-secondary-button>
             </div>
 
-            <div class="overflow-x-auto">
+            <div class="overflow-hidden">
                 <div class="grid grid-cols-1 gap-4">
                     @forelse ($patients as $patient)
                         <div class="cursor-pointer bg-white p-4 rounded-lg shadow hover:shadow-md transition-shadow group" wire:click="detail({{ $patient }})">
@@ -91,24 +91,40 @@ on(['set-date' => function ($date) {
                                 </div>
                                 <div class="flex-1 min-w-0">
                                     <div class="flex items-center justify-between">
-                                        <p class="text-sm font-medium text-gray-900 truncate">
-                                            {{ $patient->full_name }}
-                                        </p>
-                                        <div class="flex items-center space-x-2 opacity-0 group-hover:opacity-100 transition-opacity">
-                                            <button type="button" class="text-gray-600 hover:text-gray-900"
-                                                wire:click="edit({{ $patient->id }})">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                                    <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
-                                                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                                        <div class="flex items-center space-x-2">
+                                            <p class="text-sm font-medium text-gray-900 truncate">
+                                                {{ $patient->full_name }}
+                                            </p>
+                                            @if($patient->height || $patient->weight)
+                                                <span class="text-sm text-gray-500">
+                                                    ({{ $patient->height ? 'H: '.$patient->height . 'cm' : '' }}{{ $patient->height && $patient->weight ? ' / ' : '' }}{{ $patient->weight ? 'W: '.$patient->weight . 'kg' : '' }})
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div x-data="{ open: false }" class="relative">
+                                            <button type="button"
+                                                @click.stop="open = !open"
+                                                class="text-gray-600 hover:text-gray-900">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                                 </svg>
                                             </button>
-                                            <button type="button" class="text-red-600 hover:text-red-900"
-                                                wire:click="delete({{ $patient }})"
-                                                wire:confirm="Are you sure you want to delete this patient?">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                                    <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
+
+                                            <div x-show="open"
+                                                @click.away="open = false"
+                                                class="absolute -top-2 right-8 w-48 bg-white rounded-md shadow-lg z-50 border border-gray-200">
+                                                <button type="button"
+                                                    wire:click="edit({{ $patient->id }})"
+                                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left">
+                                                    Edit
+                                                </button>
+                                                <button type="button"
+                                                    wire:click="delete({{ $patient }})"
+                                                    wire:confirm="Are you sure you want to delete this patient?"
+                                                    class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full text-left">
+                                                    Delete
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="grid grid-cols-4 gap-4 mt-2 text-sm text-gray-500">
