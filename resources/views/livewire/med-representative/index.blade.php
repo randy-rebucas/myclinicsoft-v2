5aabb30f821e1e6cc0db1dc3f71cc1715d1f3611
Randy <rebucasrandy1986@gmail.com>
2024-12-03 17:58:47 +0800

    update

--- resources/views/livewire/med-representative/index.blade.php
+++ resources/views/livewire/med-representative/index.blade.php
@@ -47,7 +47,8 @@ $delete = function (MedRepresentative $medRepresentative) {
 };
 
 $detail = function (MedRepresentative $medRepresentative) {
-    $this->redirectRoute('med-representative-detail', ['medRepresentativeId' => $medRepresentative->medRepresentative]);
+    return false;
+    // $this->redirectRoute('med-representative-detail', ['medRepresentativeId' => $medRepresentative->medRepresentative]);
 };
 
 $edit = function ($id) {
@@ -121,6 +122,13 @@ $save = function () {
                                                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                         </svg>
                                         <span class="uppercase">{{ $item->medRepresentative->gender }}</span>
+                                    </p>
+                                    <p class="text-sm text-gray-600 flex items-center gap-2">
+                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                        </svg>
+                                        {{ $item->medRepresentative->user->email }}
                                     </p>
                                 </div>
                             </div>
--- resources/views/livewire/receptionist/index.blade.php
+++ resources/views/livewire/receptionist/index.blade.php
@@ -40,7 +40,9 @@ $delete = function (Receptionist $receptionist) {
 };
 
 $detail = function (Receptionist $receptionist) {
-    $this->redirectRoute('receptionist-detail', ['receptionistId' => $medRepresentative]);
+    return false;
+    // return redirect()->route('receptionist-detail', ['receptionistId' => $receptionist]);
+    // $this->redirectRoute('receptionist-detail', ['receptionistId' => $medRepresentative]);
 };
 
 $edit = function ($id) {
@@ -111,6 +113,13 @@ $save = function () {
                                                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                         </svg>
                                         <span class="uppercase">{{ $receptionist->gender }}</span>
+                                    </p>
+                                    <p class="text-sm text-gray-600 flex items-center gap-2">
+                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                        </svg>
+                                        {{ $receptionist->user->email }}
                                     </p>
                                 </div>
                             </div>
